dac625017d5ff367380c264192e364fafb93a70b
007vict <super_vg@bk.ru>
2021-08-21 13:32:31 +0300

    json-server

--- src/App.js
+++ src/App.js
@@ -11,9 +11,10 @@ function App() {
   const dispatch = useDispatch();
 
   React.useEffect(() => {
-    axios.get('http://localhost:3000/db.json').then(({ data }) => {
-      dispatch(setPizzas(data.pizzas))
-    });
+    axios.get('http://localhost:3001/pizzas').then(({ data }) => {
+      console.log(data);
+      dispatch(setPizzas(data));
+    })
   }, [])
 
   return (
