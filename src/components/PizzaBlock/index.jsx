af180c4de4b0ce8f8e5e6e841b1c5c98b45760d7
007vict <super_vg@bk.ru>
2021-08-25 15:57:04 +0300

    make cart functional

--- src/components/PizzaBlock/index.jsx
+++ src/components/PizzaBlock/index.jsx
@@ -1,59 +1,75 @@
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
 import React from 'react';
+import Button from '../Button';
 
 
 
-function PizzaBlock({ name, price,imageUrl, types, sizes }) {
-  const availableTypes = ['тонкое', 'традиционное']
-  const availableSizes = [26, 30, 40]
-  const [activeType, setActiveType] = React.useState(types[0])
-  const [activeSize, setActiveSize] = React.useState(sizes[0])
-
+function PizzaBlock({ id, name, price, imageUrl, types, sizes, onClickAddPizza, addedCount }) {
+  const availableTypes = ['тонкое', 'традиционное'];
+  const availableSizes = [26, 30, 40];
+  const [activeType, setActiveType] = React.useState(types[0]);
+  const [activeSize, setActiveSize] = React.useState(0);
 
   const onSelectType = (index) => {
-    setActiveType(index)
-  }
+    setActiveType(index);
+  };
 
   const onSelectSize = (index) => {
-    setActiveSize(index)
+    setActiveSize(index);
+  };
+
+  const onAddPizza = () => {
+    const obj = {
+      id,
+      name,
+      price,
+      imageUrl,
+      size: availableSizes[activeSize],
+      type: availableTypes[activeType],
+    }
+    onClickAddPizza(obj)
   }
 
   return (
     <div className="pizza-block">
-      <img
-        className="pizza-block__image"
-        src={imageUrl}
-        alt="Pizza"
-      />
+      <img className="pizza-block__image" src={imageUrl} alt="Pizza" />
       <h4 className="pizza-block__title">{name}</h4>
       <div className="pizza-block__selector">
         <ul>
-          {availableTypes.map((type, index) =>
+          {availableTypes.map((type, index) => (
             <li
-            key={type}
-            onClick={() => {onSelectType(index)}}
-            className={classNames({
-              'active': activeType === index,
-              'disabled': !types.includes(index),
-            })}
-            >{type}</li>)}
+              key={type}
+              onClick={() => {
+                onSelectType(index);
+              }}
+              className={classNames({
+                active: activeType === index,
+                disabled: !types.includes(index),
+              })}>
+              {type}
+            </li>
+          ))}
         </ul>
         <ul>
-        {availableSizes.map((size, index) =>
+          {availableSizes.map((size, index) => (
             <li
-            key={size}
-            onClick={() => {onSelectSize(index)}}
-            className={classNames({
-              'active': activeSize === index,
-              'disabled': !sizes.includes(size),
-            })}
-            >{size} см.</li>)}
+              key={size}
+              onClick={() => {
+                onSelectSize(index);
+              }}
+              className={classNames({
+                active: activeSize === index,
+                disabled: !sizes.includes(size),
+              })}>
+              {size} см.
+            </li>
+          ))}
         </ul>
       </div>
       <div className="pizza-block__bottom">
         <div className="pizza-block__price">от {price} ₽</div>
-        <div className="button button--outline button--add">
+        <Button onClick={onAddPizza} className="button--add" outline>
           <svg
             width="12"
             height="12"
@@ -66,8 +82,8 @@ function PizzaBlock({ name, price,imageUrl, types, sizes }) {
             />
           </svg>
           <span>Добавить</span>
-          <i>2</i>
-        </div>
+          {addedCount && <i>{addedCount}</i>}
+        </Button>
       </div>
     </div>
   );
@@ -79,6 +95,8 @@ PizzaBlock.propTypes = {
   price: PropTypes.number.isRequired,
   types: PropTypes.arrayOf(PropTypes.number).isRequired,
   sizes: PropTypes.arrayOf(PropTypes.number).isRequired,
+  onAddPizza: PropTypes.func,
+  addedCount: PropTypes.number,
 }
 
 PizzaBlock.defaultProps = {
